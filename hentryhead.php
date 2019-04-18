a83e3b23d44e21f0badd8d2d4b32cd143f1ad15b
Josh Wood <j@joshix.com>
2019-04-18 15:23:03 -0700

    Theme Test Fixes
    * style.css: Fix long entry title overflow
    
    Fixes bug shown by
    2009/10/05/title-should-not-overflow-the-content-area/
    in WP Theme test data.
    
    * hentrymeta.php: Permalink post date
    
    Fixes no-title bug shown by
    http://mywp-wpt.apps.pxdbm-345a.openshiftworkshop.com/2009/09/05/edge-case-no-title/

--- hentryhead.php
+++ hentryhead.php
@@ -6,6 +6,8 @@
  */
 ?>
 				<h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-<div class="posted"><?php _e('Posted by', 'simplish'); ?> <?php sp_byline_hcard() ?>  &ndash; 
-<abbr class="published posted_date" title="<?php the_time('Y-m-d\TH:i:sP') ?>"><?php echo get_the_time(get_option('date_format'))  /* Use get_the_time() to avoid the_date() output bug in this context. */ ?></abbr>
+				<div class="posted"><?php _e('Posted by', 'simplish'); ?> <?php sp_byline_hcard() ?>  &ndash; 
+					<a href="<?php the_permalink() ?>" rel="bookmark">
+						<abbr class="published posted_date" title="<?php the_time('Y-m-d\TH:i:sP') ?>"><?php echo get_the_time(get_option('date_format'))  /* Use get_the_time() to avoid the_date() output bug in this context. */ ?></abbr>
+					</a>
 				</div>
